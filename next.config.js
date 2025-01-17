2c4e6465320e5e29fe1f166bd5e36383e64a6eee
Tomas <tomas@tomasps.com>
2025-01-17 23:16:05 +0100

    chore: update version to 0.1.1, add next.config.js for environment variables and rewrites, and clean up auth-provider component

---
+++ next.config.js
@@ -0,0 +1,29 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    env: {
+      API_KEY: process.env.API_KEY,
+      NEXTAUTH_URL: process.env.NODE_ENV === 'development' 
+        ? 'http://localhost:3000' 
+        : process.env.NEXTAUTH_URL,
+      NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+      GITHUB_ID: process.env.GITHUB_ID,
+      GITHUB_SECRET: process.env.GITHUB_SECRET,
+    },
+    async rewrites() {
+      return [
+        {
+          source: "/api/predict/:path*",
+          destination: "http://localhost:11434/api/:path*",
+        },
+      ];
+    },
+    images: {
+      domains: ['avatars.githubusercontent.com'], // For GitHub avatar images
+    },
+    // Add experimental features for authentication
+    experimental: {
+      serverActions: true,
+    },
+  };
+  
+  module.exports = nextConfig;
